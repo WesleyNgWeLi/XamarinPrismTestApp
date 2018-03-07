d166ed99d5e3462d79a173c1ba8211ae172c93b0
Wesley Ng <wesleyngweli@gmail.com>
2018-03-07 15:45:12 -0600

    First Commit

---
+++ PrismTestApp1/PrismTestApp1/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,41 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PrismTestApp1.ViewModels
+{
+    public class MainPageViewModel : ViewModelBase
+    {
+        INavigationService _navigationService;
+
+        public DelegateCommand DoSomething => new DelegateCommand(NavigateToContentPage);
+
+        public string btnMessage
+        {
+            get { return _btnMessage; }
+            set { SetProperty(ref _btnMessage, value); }
+        }
+
+        private string _btnMessage;
+
+        public MainPageViewModel(INavigationService navigationService) 
+            : base (navigationService)
+        {
+            _navigationService = navigationService;
+            btnMessage = "Click Me!?!?";
+            Title = "Main Page";
+        }
+
+        public void NavigateToContentPage()
+        {
+            _navigationService.NavigateAsync("PrismContentPage1");
+            btnMessage = "i was clicked!";
+        }
+
+
+    }
+}
---
+++ PrismTestApp1/PrismTestApp1/Views/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace PrismTestApp1.Views
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage ()
+		{
+			InitializeComponent ();
+		}
+
+        void OnButtonClicked(object sender, EventArgs args)
+        {
+            Console.WriteLine("Button was Clicked =O Blubbbb~~~");
+        }
+    }
+}
