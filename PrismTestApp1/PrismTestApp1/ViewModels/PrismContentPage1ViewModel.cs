c44e883bbc99eb8410f139d674a5a491f95713de
Wesley Ng <wesleyngweli@gmail.com>
2018-03-08 12:19:08 -0600

    Mid Set up for content page 1

--- PrismTestApp1/PrismTestApp1/ViewModels/PrismContentPage1ViewModel.cs
+++ PrismTestApp1/PrismTestApp1/ViewModels/PrismContentPage1ViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using Prism.Commands;
 using Prism.Mvvm;
+using Prism.Navigation;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,7 +9,28 @@ namespace PrismTestApp1.ViewModels
 {
 	public class PrismContentPage1ViewModel : BindableBase
 	{
-        public PrismContentPage1ViewModel()
+
+
+        //this is how you bind a XAML Command
+        public DelegateCommand DoSomething => new DelegateCommand(NavigateToContentPage);
+
+        //this is how you bind a variable in XAML
+        public string btnMessage
+        {
+            get { return _btnMessage; }
+            set { SetProperty(ref _btnMessage, value); }
+        }
+
+        private string _btnMessage;
+
+
+        public void NavigateToContentPage()
+        {
+            _navigationService.NavigateAsync("PrismContentPage1");
+            btnMessage = "i was clicked!";
+        }
+
+        public PrismContentPage1ViewModel(INavigationService navigationService)
         {
 
         }
